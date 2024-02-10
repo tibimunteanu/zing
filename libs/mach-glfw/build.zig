27126c2f05f275413eae31b0847eb0b7e3b85b8f
tibimunteanu <sn_tibi@yahoo.com>
2024-02-11 00:15:15 +0200

    add mach-glfw as a local dep

---
+++ libs/mach-glfw/build.zig
@@ -0,0 +1,40 @@
+const builtin = @import("builtin");
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    const glfw_dep = b.dependency("glfw", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    var module = b.addModule("mach-glfw", .{
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = .{ .path = "src/main.zig" },
+    });
+    module.linkLibrary(glfw_dep.artifact("glfw"));
+    @import("glfw").addPaths(module);
+
+    const test_step = b.step("test", "Run library tests");
+    const main_tests = b.addTest(.{
+        .name = "glfw-tests",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    main_tests.linkLibrary(glfw_dep.artifact("glfw"));
+    @import("glfw").addPaths(&main_tests.root_module);
+    b.installArtifact(main_tests);
+
+    test_step.dependOn(&b.addRunArtifact(main_tests).step);
+}
+
+comptime {
+    const supported_zig = std.SemanticVersion.parse("0.12.0-dev.2063+804cee3b9") catch unreachable;
+    if (builtin.zig_version.order(supported_zig) != .eq) {
+        // @compileError(std.fmt.comptimePrint("unsupported Zig version ({}). Required Zig version 2024.1.0-mach: https://machengine.org/about/nominated-zig/#202410-mach", .{builtin.zig_version}));
+    }
+}
